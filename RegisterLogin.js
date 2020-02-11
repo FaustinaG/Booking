6ca0701304c626524cf6ebdadcbedc63f7872908
Faustina G <faustina.g@psiog.com>
2020-02-11 18:36:14 +0530

    Changes(11/2)_3

--- RegisterLogin.js
+++ RegisterLogin.js
@@ -8,11 +8,41 @@ $(document).ready(function(){
             return false;
         }
     var Password = document.getElementById("Password").value;
-    if(Password.trim()=="")
-    {
-        alert("Please enter 'Password'");
+    var ConfirmPassword = document.getElementById("ConfirmPassword").value;
+    if(Password.trim() != "" && Password == ConfirmPassword) {
+        if(Password.length < 5) {
+          alert("Password must contain at least five characters!");
+          return false;
+        }
+        if(Password == UserName) {
+          alert("Password must be different from Username!");
+          return false;
+        }
+        re = /[0-9]/;
+        if(!re.test(Password)) {
+          alert("Password must contain at least one number (0-9)!");
+          return false;
+        }
+        re = /[a-z]/;
+        if(!re.test(Password)) {
+          alert("Password must contain at least one lowercase letter (a-z)!");
+          return false;
+        }
+        re = /[A-Z]/;
+        if(!re.test(Password)) {
+          alert("Password must contain at least one uppercase letter (A-Z)!");
+          return false;
+        }
+        re = /[!@#$%^&*]/;
+        if(!re.test(Password)) {
+          alert("Password must contain at least one special character within (!@#$^&*)!");
+          return false;
+        }
+      } 
+      else {
+        alert("Please check that you've entered and confirmed your password!");
         return false;
-    }
+      }
         var userdetail = {
         UserName : UserName,
         Password : Password,
